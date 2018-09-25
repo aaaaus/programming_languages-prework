c32fd3dca4aa4a51be85f1788aeb70fe3bede5b4
Austin Luft <ausluft@gmail.com>
2018-09-25 22:10:32 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages[:oo].keys.each do |key|
     if key == :ruby 
-      languages[:oo][:ruby] << {style: "oo" }
+      languages[:oo][:ruby] = {style: "oo" }
     end
   end
   new_hash << languages[:oo]
